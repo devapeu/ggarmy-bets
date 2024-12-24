2f7900d8f73c84d3dbd14b18e1c99d3fa2edffdc
Piero del Valle <devapeu@gmail.com>
2024-12-23 20:10:02 -0500

    update database columns

--- api/database.sql
+++ api/database.sql
@@ -5,8 +5,8 @@ USE `challonge`;
 CREATE TABLE IF NOT EXISTS `votes` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
     `match_id` INT NOT NULL,
-    `tournament_id` INT NOT NULL,
-    `ip` VARCHAR(45) NOT NULL,
+    `tournament_id` VARCHAR(255) NOT NULL,
+    `ip` VARCHAR(64) NOT NULL,
     `voted_for_player_id` INT,
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
