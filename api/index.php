da9f5121902026b3411dc1c496c26c833a73cce9
Piero del Valle <devapeu@gmail.com>
2024-12-23 23:53:18 -0500

    update headers

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/votes.php';
 
 header('Access-Control-Allow-Origin: ' . $FRONTEND_URL);
-header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json');
 
